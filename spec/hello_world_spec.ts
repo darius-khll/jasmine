4cbe52f6d052e3fc3b4b4235c9ce11c5c7507731
Ali Khalili <ali.khll.1994@gmail.com>
2017-01-02 18:03:05 +0330

    hello world spec added

---
+++ spec/hello_world_spec.ts
@@ -0,0 +1,22 @@
+import * as request from "request";
+import * as helloWorld from "../index.js"
+var base_url = "http://localhost:3000/"
+
+describe("Hello World Server", function () {
+    describe("GET /", function () {
+        it("returns status code 200", function (done) {
+            request.get(base_url, function (error, response, body) {
+                expect(response.statusCode).toBe(200);
+                done();
+            });
+        });
+
+        it("returns Hello World", function (done) {
+            request.get(base_url, function (error, response, body) {
+                expect(body).toBe("Hello World");
+                helloWorld.closeServer();
+                done();
+            });
+        });
+    });
+});
