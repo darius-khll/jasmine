51aeade812c2ef5a2fb0db277fe671f388389e65
Ali Khalili <ali.khll.1994@gmail.com>
2017-01-21 15:05:26 +0330

    default

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 "use strict";
-const request = require("request");
 const express = require("express");
 var app = express();
 app.get('/', (req, res) => {
@@ -12,16 +11,4 @@ function closeServer() {
     server.close();
 }
 exports.closeServer = closeServer;
-class CustomRequest {
-    static get(uri) {
-        return new Promise((res, rej) => {
-            request.get(uri, (error, response, body) => {
-                console.log(body);
-                let retObj = { body: body, error: error, response: response };
-                return res(retObj);
-            });
-        });
-    }
-}
-exports.CustomRequest = CustomRequest;
 //# sourceMappingURL=index.js.map
