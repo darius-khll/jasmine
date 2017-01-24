5dfdf1b8dd7b2dd78c4c45a84efad6f3d57f49c2
Ali Khalili <ali.khll.1994@gmail.com>
2017-01-24 19:54:10 +0330

    container component router enhanced

--- public/app/container.ts
+++ public/app/container.ts
@@ -2,7 +2,7 @@
 var app = angular.module('app', ["ngComponentRouter"]);
 
 
-app.config(function($locationProvider) {
+app.config(function ($locationProvider) {
   $locationProvider.html5Mode(true);
 })
 
@@ -10,28 +10,29 @@ app.value('$routerRootComponent', 'app')
 
 app.component('app', {
   template:
-    '<nav>\n' +
-    '  <a ng-link="[\'CrisisCenter\']">Crisis Center</a>\n' +
-    '  <a ng-link="[\'Heroes\']">Heroes</a>\n' +
-    '</nav>\n' +
-    '<ng-outlet></ng-outlet>\n',
+  `<nav>
+      <a ng-link="['CrisisCenter']">Crisis Center</a>
+      <a ng-link="['Heroes']">Heroes</a>
+      
+    </nav>
+    <ng-outlet></ng-outlet>`,
   $routeConfig: [
-    {path: '/crisis-center/...', name: 'CrisisCenter', component: 'crisisCenter', useAsDefault: true},
-    {path: '/heroes/...', name: 'Heroes', component: 'heroes' }
+    { path: '/crisis-center', name: 'CrisisCenter', component: 'crisisCenter', useAsDefault: true },
+    {path: '/heroes', name: 'Heroes', component: 'heroes' }
   ]
 });
 
-
-
+app.component('crisisCenter', {template: "<h2>Crisis component</h2>"});
+app.component('heroes', {template: "<h2>Heroes component</h2>"});
 
 //////////////////////////////////////////////////////////////////////
 
 class Ctrl {
-    public Name = 'Ali';
+  public Name = 'Ali';
 }
 
 app.component('ctrl',
-    {
-        controller: Ctrl, template: `<h1>this is from component by {{vm.Name}}</h1>`, controllerAs: 'vm'
-    });
+  {
+    controller: Ctrl, template: `<h1>this is from component by {{vm.Name}}</h1>`, controllerAs: 'vm'
+  });
 
