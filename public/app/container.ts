ddbef796a4a8efce5da9e635fc3d4158ad7e0f7f
Ali Khalili <ali.khll.1994@gmail.com>
2017-01-25 13:06:58 +0330

    container modified

--- public/app/container.ts
+++ public/app/container.ts
@@ -11,28 +11,15 @@ app.value('$routerRootComponent', 'app')
 app.component('app', {
   template:
   `<nav>
-      <a ng-link="['CrisisCenter']">Crisis Center</a>
+      <a ng-link="['First']">First</a>
       <a ng-link="['Heroes']">Heroes</a>
       
     </nav>
     <ng-outlet></ng-outlet>`,
   $routeConfig: [
-    { path: '/crisis-center', name: 'CrisisCenter', component: 'crisisCenter', useAsDefault: true },
+    { path: '/first', name: 'First', component: 'first', useAsDefault: true },
     { path: '/heroes', name: 'Heroes', component: 'heroes' }
   ]
 });
 
-app.component('crisisCenter', { template: "<h2>Crisis component</h2>" });
-app.component('heroes', { template: "<h2>Heroes component</h2>" });
-
-//////////////////////////////////////////////////////////////////////
-
-class Ctrl {
-  public Name = 'Ali';
-}
-
-app.component('ctrl',
-  {
-    controller: Ctrl, template: `<h1>this is from component by {{vm.Name}}</h1>`, controllerAs: 'vm'
-  });
 
