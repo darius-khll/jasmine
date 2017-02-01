de96e10dfa7966e2ec929c27c5a0771194c840a8
Ali Khalili <ali.khll.1994@gmail.com>
2017-02-01 14:04:55 +0330

    first component import another service

--- public/app/component/firstComponent.ts
+++ public/app/component/firstComponent.ts
@@ -1,40 +1,45 @@
 
-app = angular.module('app');
+import Service = ClientApp.Service;
 
+module ClientApp {
+    export module Component {
+        app = angular.module('app');
 
-class FirstComponent {
+        export class FirstComponent {
+            public $router: ng.Router;
 
-    public $router: ng.Router;
+            constructor(private $http: ng.IHttpService, private firstService: Service.FirstService) {
+                console.warn(this.firstService.getFirstServiceRecords());
+            }
 
-    constructor(private $http: ng.IHttpService, private firstService: FirstService) {
-        console.warn(this.firstService.getFirstServiceRecords());
-    }
+            public async $onInit() {
+                console.log("started...");
+            }
 
-    public async $onInit() {
-        console.log("started...");
-    }
+            public async check(): Promise<void> {
+                let records: any = await this.$http.get("/getRecords");
+                alert(records.data[0]);
+                this.goToSecondComponent();
+            }
 
-    public async check(): Promise<void> {
-        let records: any = await this.$http.get("/getRecords");
-        alert(records.data[0]);
-        this.goToSecondComponent();
-    }
+            public goToSecondComponent() {
+                this.$router.navigate(['Second']);
+            }
 
-    public goToSecondComponent() {
-        this.$router.navigate(['Second']);
-    }
+            public async $onDestroy() {
+                console.log("finished...");
+            }
+        }
 
-    public async $onDestroy() {
-        console.log("finished...");
+        FirstComponent.$inject = ['$http', 'firstService'];
+
+        app.component('first',
+            {
+                templateUrl: "app/view/firstView.html",
+                controller: FirstComponent,
+                controllerAs: 'vm',
+                bindings: { $router: '<' }
+            });
+
     }
 }
-
-FirstComponent.$inject = ['$http', 'firstService'];
-
-app.component('first',
-    {
-        templateUrl: "app/view/firstView.html",
-        controller: FirstComponent,
-        controllerAs: 'vm',
-        bindings: { $router: '<' }
-    });
